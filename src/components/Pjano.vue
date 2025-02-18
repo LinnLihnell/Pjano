5382a5d1d476910cb42c59c108a48185508bef1b
linn <lihnelllinn@gmail.com>
2025-02-18 09:58:16 +0100

    added component to home

--- src/components/Pjano.vue
+++ src/components/Pjano.vue
@@ -1,5 +1,7 @@
 <script></script>
 
-<template></template>
+<template>
+  hej
+</template>
 
 <style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,14 @@
-<template><h1>Home</h1></template>
+<script>
+import Pjano from '../components/Pjano.vue'
+
+export default{
+  components:{
+    Pjano
+  }
+}
+</script>
+
+<template>
+<h1>Home</h1>
+<Pjano/>
+</template>
