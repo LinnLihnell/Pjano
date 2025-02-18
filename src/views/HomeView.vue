cd3a36c6dad9c022a18fa639ce90aa936a042209
linn <lihnelllinn@gmail.com>
2025-02-18 10:12:45 +0100

    added logo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,22 @@
 <script>
-import Pjano from '../components/Pjano.vue'
+import Pjano from '../components/Pjano.vue';
 
 export default{
-  components:{
+  components: {
     Pjano
+  },
+  data(){
+    return{
+      logga: '../assets/Pjanologga.png'
+    }
+
   }
 }
+
 </script>
 
 <template>
+  <img :src=logga alt="">
 <h1>Home</h1>
 <Pjano/>
 </template>
