9c3d065cb7d40949b04e703f8f802e1640bbbc84
linn <lihnelllinn@gmail.com>
2025-02-18 14:27:32 +0100

    cssdelete

--- src/App.vue
+++ src/App.vue
@@ -17,39 +17,70 @@ import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
-  <div id="header">
+  <section>
     <img :src=logga alt="logo">
-    <nav>
+    <nav class="navbar">
       <ul>
         <li><RouterLink to="/">Home</RouterLink></li>
         <li> <RouterLink to="/profile">Profile</RouterLink></li>
       </ul>
     </nav>
+  </section>
 
-  </div>
 <main>
   <RouterView/>
 </main>
 
+<footer>
+  <p>Grupp 14&copy 2025</p>
+</footer>
+
 </template>
 
 <style scoped>
-#header{
+section{
 background-color:#d9d9d9 ;
 text-align: center;
+margin: 0;
+padding: 0;
+/* height: 100vh; */
+/* width: 980px */
+/* max-width: 100% */
+}
+main{
+  width: 100vw;
+  /* height: 100vh; */
 }
 img{
 height: 150px;
 }
 nav{
-  background-color:#9ac5cb
+  background-color:#474554;
+  padding: 10px;
 }
-ul{
+.navbar ul{
+  margin: 0;
   list-style-type: none;
   display: flex;
-
+  padding: 0;
+  overflow: hidden;
 }
-a{
-text-decoration: none
+.navbar a{
+text-decoration: none;
+color:white;
+margin: 5px;
+display: block;
+}
+
+.navbar a:hover {
+  background-color: blue;
+}
+
+footer{
+  background-color: #474554;
+  color: white;
+  text-align: center;
+  padding: 2px;
+  margin-top:10px;
 }
 </style>
