2f944a1cdf6c749da3bf2cac65716cec2d99cda9
HampusLarss0n <hlarsson0616@gmail.com>
2025-02-20 11:06:43 +0100

    Question conponent

---
+++ src/components/Questions.vue
@@ -0,0 +1,28 @@
+<script>
+export default{
+  data(){
+    return{questions:[
+      {question:"press key C",answer:"C"},
+      {question:"press key C#",answer:"C#"},
+      {question:"press key D",answer:"D"},
+      {question:"press key D#",answer:"D#"},
+      {question:"press key E",answer:"E"},
+      {question:"press key F",answer:"F"},
+      {question:"press key F#",answer:"F#"},
+      {question:"press key G",answer:"G"},
+      {question:"press key G#",answer:"G#"},
+      {question:"press key A",answer:"A"},
+      {question:"press key A#",answer:"A#"},
+      {question:"press key B",answer:"B"},]}
+  }
+}
+
+</script>
+
+
+<template>
+<p v-for="x in questions.question"></p>
+</template>
+
+
+<style></style>
