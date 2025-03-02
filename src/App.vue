baafa7beffdf9cfd4bc9c07f081da8f6866593ac
Pontus Lindahl <plindahl@ourmagnet.net>
2025-03-02 19:38:25 +0100

    update logo and footer positioning

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,8 @@ export default {
 
 <template>
   <section>
-    <img :src=logga alt="logo" id="logo">
     <nav class="navbar">
+      <img :src=logga alt="logo" id="logo">
       <ul>
         <li>
           <RouterLink to="/">Home</RouterLink>
@@ -51,6 +51,15 @@ body {
   ;
 }
 
+footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  margin-top: 10px;
+}
+
 section {
   /* background-color:#d9d9d9 ; */
   text-align: center;
@@ -67,71 +76,68 @@ main {
 }
 
 
-#logo{
+#logo {
   height: 150px;
   border-radius: 50%;
-} 
+  margin: 0 0 -75px 10px;
+}
 
 nav {
   background-color: #474554;
 }
-@media(min-width: 501px){
-.navbar ul {
-  margin: 0px;
+
+nav ul {
+  margin: 0 auto 0 10px;
   list-style-type: none;
   padding: 0px;
   overflow: hidden;
   background-color: #474554;
 }
 
-.navbar a {
-  text-decoration: none;
-  text-align: center;
-  color: white;
-  margin: 5px;
-  padding: 8px;
-  display: block;
-}
+@media(min-width: 501px) {
+  .navbar a {
+    text-decoration: none;
+    text-align: center;
+    color: white;
+    margin: 5px;
+    padding: 8px;
+    display: block;
+  }
 
-.navbar li:hover {
-  background-color: black;
+  .navbar li:hover {
+    background-color: black;
+    border-radius: 15%;
+  }
 
-}
+  .navbar li {
+    float: left;
+    display: inline;
+  }
 
-.navbar li {
-  float: left;
-  display: inline;
-}
-
-footer {
-  /* background-color: #474554;
-  color: white; */
-  text-align: center;
-  /* padding: 2px;
-  margin-top:10px; */
-}
-
-#piano {
-  display: flex;
-  justify-content: center;
-} 
+  #piano {
+    display: flex;
+    justify-content: center;
+  }
 }
 
 @media(max-width: 500px) {
-#logo {
-  display: none;
-} 
-footer {
-  display: none;
-}
-body{
-  transform: rotate(90deg);
-}
-.navbar ul {
-  margin: 0px;
-  list-style-type: none;
-  padding: 0px;
-  overflow: hidden;
-}
+  #logo {
+    display: none;
+  }
+
+  footer {
+    display: none;
+  }
+
+  body {
+    transform: rotate(90deg);
+  }
+
+  .navbar ul {
+    margin: 0px;
+    list-style-type: none;
+    padding: 0px;
+    overflow: hidden;
+  }
 }
 </style>
