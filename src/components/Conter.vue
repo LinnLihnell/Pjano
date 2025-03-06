9d333d5d6b6be4b089517cb9ccd6ce878118bbb7
HampusLarss0n <hlarsson0616@gmail.com>
2025-03-06 13:36:00 +0100

    completed conter.vue

--- src/components/Conter.vue
+++ src/components/Conter.vue
@@ -1,6 +1,38 @@
-<script></script>
+<script>
+export default{
 
-<template></template>
+  data() {
+    return {
+      conter:0,
+      display:[],
+    }
+  },
+  methods:{
+    learn_more(){
+      if(this.feedback==="rätt"){
+        this.conter = 0
+      } else{
+        this.conter = this.conter +1
+        if(this.conter>=2){
+          this.display[0] = this.chord + ", " +this.display[0]
+        }
+      }
+    },
+  },
+  props: {
+    chord: {
+      type: String
+    },
+    feedback: {
+      type: String
+    },
+  }
+}
+</script>
+
+<template>
+  <p>{{ display }}</p>
+</template>
 
 <style></style>
 
